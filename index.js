ab68a4804229f1c04354a200a6ae3da25b57dada
Felipe <felipe@visionar.io>
2024-04-29 18:36:34 -0300

    Add server libraries

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var express = require('express')
-var app = express()
+const os = require('os');
+const cluster = require('cluster');
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
 
-app.get('/', function (req, res) {
-    res.send('HS Server')
-})
+if (cluster.isMaster) {
+    for (var i = 0, n = os.cpus().length; i < n; i += 1) {
+        cluster.fork();
+    }
+}
+else {
+    application();
+}
 
-app.listen(3000)
+function application() {
+    const app = express()
+    app.use(compression());
+
+    app.get('/', function (req, res) {
+        res.send('HS Server')
+    })
+
+    app.listen(8080, function () {
+        console.log('app:8080');
+    });
+}
+
